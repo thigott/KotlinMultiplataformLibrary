85bb66ce8de181df3294ad01b2b1f866d100c889
thiagoalves <thiagoalves@ioasys.com.br>
2023-05-24 14:31:48 -0300

    Removendo threadContextProvider

--- domain/src/commonMain/kotlin/com/thigott/kotlinmultiplataformlibrary/domain/core/UseCase.kt
+++ domain/src/commonMain/kotlin/com/thigott/kotlinmultiplataformlibrary/domain/core/UseCase.kt
@@ -1,15 +1,15 @@
 package com.thigott.kotlinmultiplataformlibrary.domain.core
 
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
 
 abstract class UseCase<T, in Params>(private val scope: CoroutineScope): KoinComponent {
-    private val contextProvider: ThreadContextProvider by inject()
 
     abstract fun run(params: Params? = null): Flow<T>
 
@@ -18,15 +18,19 @@ abstract class UseCase<T, in Params>(private val scope: CoroutineScope): KoinCom
         onError: ((Throwable) -> Unit) = {},
         onSuccess: (T) -> Unit = {}
     ) {
-        scope.launch(contextProvider.io) {
+        scope.launch(
+            CoroutineExceptionHandler { _, error ->
+                onError.invoke(error)
+            }
+        ) {
             try {
                 run(params).collect {
-                    withContext(contextProvider.main) {
+                    withContext(Dispatchers.Main) {
                         onSuccess(it)
                     }
                 }
             } catch (e: Exception) {
-                withContext(contextProvider.main) {
+                withContext(Dispatchers.Main) {
                     onError(e)
                 }
             }
--- shared/src/commonMain/kotlin/com/thigott/kotlinmultiplataformlibrary/di/domainModule.kt
+++ shared/src/commonMain/kotlin/com/thigott/kotlinmultiplataformlibrary/di/domainModule.kt
@@ -1,16 +1,15 @@
 package com.thigott.kotlinmultiplataformlibrary.di
 
-import com.thigott.kotlinmultiplataformlibrary.domain.core.getThreadContextProvider
 import com.thigott.kotlinmultiplataformlibrary.domain.usecases.GetKtorTestUseCase
 import com.thigott.kotlinmultiplataformlibrary.domain.usecases.LoginUseCase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import org.koin.dsl.module
 
 val domainModule = module {
     single {
-        CoroutineScope(context = Dispatchers.Default)
-        getThreadContextProvider()
+        CoroutineScope(Dispatchers.Default + SupervisorJob())
     }
 
     factory {
