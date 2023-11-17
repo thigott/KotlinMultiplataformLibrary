e390577ca4ee0c1ea51c4c067439d1ef55f04c06
thiagoalves <thiago.alves@pura.com>
2023-11-17 17:39:09 -0300

    Updating KMP version to 1.9.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.library").version("8.1.1").apply(false)
-    kotlin("multiplatform").version("1.8.10").apply(false)
+    kotlin("multiplatform").version("1.9.20").apply(false)
     id("maven-publish")
     id("org.jetbrains.kotlin.plugin.serialization").version("1.8.21")
 }
